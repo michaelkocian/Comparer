df5e6b272e51e2dec6b21e8106b7213848528a18
michaelkocian <50811306+michaelkocian@users.noreply.github.com>
2020-03-30 18:16:11 +0200

    added fullscreenimage and interactive image projects

---
+++ Comparer.FullscreenImage/Form1.cs
@@ -0,0 +1,26 @@
+﻿using Cyotek.Windows.Forms;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Comparer.FullscreenImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+            var imbox = new ImageBox();
+            imbox.Size = new System.Drawing.Size(500, 500);
+            imbox.Image = Bitmap.FromFile(@"D:\zaloha telefonu\Camera\20170903_222301.jpg");
+            imbox.Dock = DockStyle.Fill;
+            this.Controls.Add(imbox);
+
+
+        }
+
+    }
+}
